3963cb9a85800bdb62fe8fcaa16a1eda68506abe
Quan <21013237@st.phenikaa-uni.edu.vn>
2023-07-19 12:13:14 +0700

    A

--- Tổng hợp/chitietphim.php
+++ Tổng hợp/chitietphim.php
@@ -1,9 +1,7 @@
 <?php
 // Kết nối đến cơ sở dữ liệu
 include 'db_connection.php';
-
 session_start();
-
 // Kết nối đến cơ sở dữ liệu
 
 // Kiểm tra xem user_id đã tồn tại trong $_SESSION hay chưa
@@ -12,18 +10,6 @@ if (isset($_SESSION['user_id'])) {
 } else {
     $user_id = null;
 }
-
-
-if ($_SESSION['user_id'] !== null) {
-    $user_id = $_SESSION['user_id'];
-    $sql = "SELECT * FROM users WHERE id = '$user_id'";
-    $result = $connection->query($sql);
-
-    if ($result->num_rows > 0) {
-        $user = $result->fetch_assoc();
-    }
-}
-
 
 // Kiểm tra xem id phim đã được truyền vào hay chưa
 if (isset($_GET['id'])) {
@@ -66,12 +52,15 @@ if (isset($_GET['id'])) {
 }
 
 // Kiểm tra xem user_id đã tồn tại trong $_SESSION hay chưa
-if (isset($_SESSION['user_id'])) {
+if ($_SESSION['user_id'] !== null) {
     $user_id = $_SESSION['user_id'];
-} else {
-    $user_id = null;
-}
+    $sql = "SELECT * FROM users WHERE id = '$user_id'";
+    $result = $connection->query($sql);
 
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+    }
+}
 
 
 // Kiểm tra xem id phim đã được truyền vào hay chưa
@@ -222,78 +211,63 @@ if (isset($_GET['id'])) {
             <header>
                 <div class="nav container">
 
-                    <a href="Trangchu.php?user_id=<?php echo $_SESSION['user_id']; ?>" class="logo">
+                <a href="Trangchu.php?user_id=<?php echo $_SESSION['user_id']; ?>" class="logo">
                         Movie<span>Manhwa</span>
                     </a>
                     <div class="search-box">
-                        <form method="post" style="display: flex;">
+                        <form method="post" action="Search.php" style="display: flex;">
                             <input type="text" name="noidung" autocomplete="off" id="search-input" placeholder="Search Movies">
-                            <button class="search-button" type="submit" name="btn">
-                                <a href="Search.html"><i class="bx bx-search"></i> </a>
+                            <button style="background-color: #2D2E37; border: none;" class="search-button" type="submit" name="btn">
+                                <i class='bx bx-search'></i>
                             </button>
                         </form>
                     </div>
 
-                    <a href="#" class="user">
-                        <img src="img/images.png" alt="" class="user-img">
+                    <a href="<?php echo isset($_SESSION['user_id']) ? 'UserInfo.php?user_id=' . $_SESSION['user_id'] : 'Dangnhap.php'; ?>" class="user">
+                        <img src="<?php echo $user !== null ? $user['avatar_link'] : 'img/images.png'; ?>" alt="" class="user-img">
                     </a>
 
                     <div class="navbar">
-
-                        <a href="TrangChu.html" class="nav-link">
-                            <i class="bx bx-home"></i>
+                        <a href="Trangchu.php?user_id=<?php echo $_SESSION['user_id']; ?>" class="nav-link">
+                            <i class="bx bx-home nav-link-icon"></i>
                             <span class="nav-link-title">Trang chủ</span>
                         </a>
-
                         <a href="#home" class="nav-link">
-                            <i class="bx bxs-hot"></i>
+                            <i class="bx bxs-hot nav-link-icon"></i>
                             <span class="nav-link-title">Thịnh hành</span>
                         </a>
-
-                        <a href="PhimBo.php" class="nav-link">
-                            <i class="bx bxs-movie"></i>
+                        <a href="PhimBo.php?user_id=<?php echo $_SESSION['user_id']; ?>" class="nav-link nav-active">
+                            <i class="bx bxs-movie nav-link-icon"></i>
                             <span class="nav-link-title">Phim bộ</span>
                         </a>
-
-                        <a href="PhimLe.php" class="nav-link">
-                            <i class="bx bxs-film"></i>
+                        <a href="PhimLe.php?user_id=<?php echo $_SESSION['user_id']; ?>" class="nav-link">
+                            <i class="bx bxs-film nav-link-icon"></i>
                             <span class="nav-link-title">Phim lẻ</span>
                         </a>
                         <div class="dropdown-toggle-container" id="genre-dropdown-toggle">
-                <a href="#" class="nav-link dropdown ">
-                    <i class="bx bx-category nav-link-icon"></i>
-                    <span class="nav-link-title">Thể loại</span>
-                 </a>
-                 <div class="dropdown-content">
-                 <div class="column">
-                     <a href="Theloai.php?genre=Hài hước">Hài hước</a>
-                     <a href="Theloai.php?genre=Hành động">Hành động</a>
-                     <a href="Theloai.php?genre=Phiêu lưu">Phiêu lưu</a>
-                     <a href="Theloai.php?genre=Tình cảm">Tình cảm</a>
-                     <a href="Theloai.php?genre=Học đường">Học đường</a>
-                     <a href="Theloai.php?genre=Võ thuật">Võ thuật</a>
-                     <a href="Theloai.php?genre=Tài liệu">Tài liệu</a>
-         
-                 </div>
-                 <div class="column">
-                     <a href="Theloai.php?genre=Viễn tưởng">Viễn tưởng</a>
-                     <a href="Theloai.php?genre=Hoạt hình">Hoạt hình</a>
-                     <a href="Theloai.php?genre=Thể thao">Thể thao</a>
-                     <a href="Theloai.php?genre=Âm nhạc">Âm nhạc</a>
-                     <a href="Theloai.php?genre=Gia đình">Gia đình</a>
-                     <a href="Theloai.php?genre=Kinh dị">Kinh dị</a>
-                     <a href="Theloai.php?genre=Tâm lý">Tâm lý</a>
-                 </div>
-                 <!-- Thêm các thể loại khác tương ứng với các option -->
-             </div>
-         
-             </div>
-
-                        <a href="#home" class="nav-link">
-                            <i class="bx bx-heart"></i>
-                            <span class="nav-link-title">Yêu thích</span>
-                        </a>
-
+                            <a href="#" class="nav-link dropdown">
+                                <i class="bx bx-category nav-link-icon"></i>
+                                <span class="nav-link-title">Thể loại</span>
+                            </a>
+                    <div class="dropdown-content">
+                        <div class="column">
+                            <a href="Theloai.php?genre=Hài hước&user_id=<?php echo $_SESSION['user_id']; ?>">Hài hước</a>
+                            <a href="Theloai.php?genre=Hành động&user_id=<?php echo $_SESSION['user_id']; ?>">Hành động</a>
+                            <a href="Theloai.php?genre=Phiêu lưu&user_id=<?php echo $_SESSION['user_id']; ?>">Phiêu lưu</a>
+                            <a href="Theloai.php?genre=Tình cảm&user_id=<?php echo $_SESSION['user_id']; ?>">Tình cảm</a>
+                            <a href="Theloai.php?genre=Học đường&user_id=<?php echo $_SESSION['user_id']; ?>">Học đường</a>
+                            <a href="Theloai.php?genre=Võ thuật&user_id=<?php echo $_SESSION['user_id']; ?>">Võ thuật</a>
+                            <a href="Theloai.php?genre=Tài liệu&user_id=<?php echo $_SESSION['user_id']; ?>">Tài liệu</a>
+                        </div>
+                        <div class="column">
+                            <a href="Theloai.php?genre=Viễn tưởng&user_id=<?php echo $_SESSION['user_id']; ?>">Viễn tưởng</a>
+                            <a href="Theloai.php?genre=Hoạt hình&user_id=<?php echo $_SESSION['user_id']; ?>">Hoạt hình</a>
+                            <a href="Theloai.php?genre=Thể thao&user_id=<?php echo $_SESSION['user_id']; ?>">Thể thao</a>
+                            <a href="Theloai.php?genre=Âm nhạc&user_id=<?php echo $_SESSION['user_id']; ?>">Âm nhạc</a>
+                            <a href="Theloai.php?genre=Gia đình&user_id=<?php echo $_SESSION['user_id']; ?>">Gia đình</a>
+                            <a href="Theloai.php?genre=Kinh dị&user_id=<?php echo $_SESSION['user_id']; ?>">Kinh dị</a>
+                            <a href="Theloai.php?genre=Tâm lý&user_id=<?php echo $_SESSION['user_id']; ?>">Tâm lý</a>
+                        </div>
                     </div>
                 </div>
                 <a href="#home" class="nav-link">
@@ -307,7 +281,7 @@ if (isset($_GET['id'])) {
 
             <section class="movie-details container">
 
-                <div class="poster-info-container">
+            <div class="poster-info-container">
                     <div class="movie-poster">
                     <a href="XemTrailer.php?id=<?php echo $row['id']; ?>&user_id=<?php echo $_SESSION['user_id']; ?>">
                             <img src="<?php echo $row['image']; ?>" alt="Movie Poster">
